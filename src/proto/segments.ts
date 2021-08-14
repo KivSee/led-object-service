ef58663d7c8324c7361b673844efc858ea4467a9
amir <amirgiraffe@gmail.com>
2021-08-14 15:41:07 +0300

    fix: ts-proto esModuleInterop

--- src/proto/segments.ts
+++ src/proto/segments.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
-import { util, configure, Writer, Reader } from "protobufjs/minimal";
-import * as Long from "long";
+import Long from "long";
+import _m0 from "protobufjs/minimal";
 
 export const protobufPackage = "";
 
@@ -27,7 +27,10 @@ export interface SegmentsMapConfig {
 const baseSegmentConfig: object = { name: "", indices: 0 };
 
 export const SegmentConfig = {
-  encode(message: SegmentConfig, writer: Writer = Writer.create()): Writer {
+  encode(
+    message: SegmentConfig,
+    writer: _m0.Writer = _m0.Writer.create()
+  ): _m0.Writer {
     if (message.name !== "") {
       writer.uint32(10).string(message.name);
     }
@@ -39,8 +42,8 @@ export const SegmentConfig = {
     return writer;
   },
 
-  decode(input: Reader | Uint8Array, length?: number): SegmentConfig {
-    const reader = input instanceof Reader ? input : new Reader(input);
+  decode(input: _m0.Reader | Uint8Array, length?: number): SegmentConfig {
+    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
     let end = length === undefined ? reader.len : reader.pos + length;
     const message = { ...baseSegmentConfig } as SegmentConfig;
     message.indices = [];
@@ -115,7 +118,10 @@ export const SegmentConfig = {
 const baseSegmentsMapConfig: object = { guid: 0, numberOfPixels: 0 };
 
 export const SegmentsMapConfig = {
-  encode(message: SegmentsMapConfig, writer: Writer = Writer.create()): Writer {
+  encode(
+    message: SegmentsMapConfig,
+    writer: _m0.Writer = _m0.Writer.create()
+  ): _m0.Writer {
     if (message.guid !== 0) {
       writer.uint32(13).fixed32(message.guid);
     }
@@ -128,8 +134,8 @@ export const SegmentsMapConfig = {
     return writer;
   },
 
-  decode(input: Reader | Uint8Array, length?: number): SegmentsMapConfig {
-    const reader = input instanceof Reader ? input : new Reader(input);
+  decode(input: _m0.Reader | Uint8Array, length?: number): SegmentsMapConfig {
+    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
     let end = length === undefined ? reader.len : reader.pos + length;
     const message = { ...baseSegmentsMapConfig } as SegmentsMapConfig;
     message.segments = [];
@@ -229,9 +235,7 @@ export type DeepPartial<T> = T extends Builtin
   ? { [K in keyof T]?: DeepPartial<T[K]> }
   : Partial<T>;
 
-// If you get a compile-error about 'Constructor<Long> and ... have no overlap',
-// add '--ts_proto_opt=esModuleInterop=true' as a flag when calling 'protoc'.
-if (util.Long !== Long) {
-  util.Long = Long as any;
-  configure();
+if (_m0.util.Long !== Long) {
+  _m0.util.Long = Long as any;
+  _m0.configure();
 }
