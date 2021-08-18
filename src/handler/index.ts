0466848a48d0bc79c99e2724dbb0a9a111556dbb
amir <amirgiraffe@gmail.com>
2021-08-18 21:43:21 +0300

    feat: automatic calculate guid for object

--- src/handler/index.ts
+++ src/handler/index.ts
@@ -1,10 +1,16 @@
 import { SegmentsMapConfig } from "../proto/segments";
 import { createStorageBackend } from "../services/storage";
+import hash from 'object-hash';
+import pino from 'pino';
 import { sendToMqtt } from '../services/mqtt';
 
 const storage = createStorageBackend();
+const logger = pino({ name: 'led object handler' });
 
 export const updateSegmentsMap = async (thingName: string, segmentsMap: SegmentsMapConfig) => {
+    const guid = parseInt(hash.sha1(segmentsMap).substr(0, 8), 16);
+    segmentsMap.guid = guid;
+    logger.info('creating or updating led object', { guid, thingName });
     await storage.upsert(thingName, segmentsMap);
     // sendToMqtt(thingName, segmentsMap.guid);
 }
