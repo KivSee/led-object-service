2956d8ce17009bd1ab9671fad533409c6217f2f8
Amir Blum <amirgiraffe@gmail.com>
2023-03-03 19:28:48 +0200

    fix: increase express parse limit

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const logger = pino();
 const app = express();
 app.use(httpLogger());
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '10mb'}));
 app.use('/thing', thingsRouter);
 app.use('/health-check', (_req, res) => {
   res.sendStatus(200);
